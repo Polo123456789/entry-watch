2f3f53bf855cf9a7055a5f17897bed67e73945c3
Polo123456789 <pabsan02@gmail.com>
2025-11-23 20:01:54 -0600

    + wrap handlers for easy error managment

---
+++ internal/http/util/handler.go
@@ -0,0 +1,62 @@
+package util
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com/Polo123456789/entry-watch/internal/app"
+)
+
+func Handler(
+	logger *slog.Logger,
+	h func(w http.ResponseWriter, r *http.Request) error,
+) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := h(w, r)
+
+		if err == nil {
+			return
+		} else if e, ok := errorAs[*ErrorWithCode](err); ok {
+			http.Error(w, e.msg, e.code)
+		} else if e, ok := errorAs[app.ForbiddenError](err); ok {
+			http.Error(w, e.Error(), http.StatusForbidden)
+		} else if e, ok := errorAs[app.UnauthorizedError](err); ok {
+			http.Error(w, e.Error(), http.StatusUnauthorized)
+		} else {
+			// TODO: Get request ID from context
+			logger.LogAttrs(
+				r.Context(),
+				slog.LevelError,
+				"internal server error",
+				slog.String("error", err.Error()),
+			)
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+		}
+	})
+}
+
+type ErrorWithCode struct {
+	msg  string
+	code int
+}
+
+func NewErrorWithCode(msg string, code int) *ErrorWithCode {
+	return &ErrorWithCode{
+		msg:  msg,
+		code: code,
+	}
+}
+
+func (e *ErrorWithCode) Error() string {
+	return e.msg
+}
+
+func errorAs[T any](err error) (T, bool) {
+	var target T
+	if errors.As(err, &target) {
+		return target, true
+	}
+	var zero T
+	return zero, false
+}
