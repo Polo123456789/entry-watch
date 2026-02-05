5037950db7eea43fc1b86cb3865a3b2cd0568ae2
Polo123456789 <pabsan02@gmail.com>
2026-02-05 10:02:50 -0600

    - clear slop

--- internal/sqlc/unmarshall.go
+++ internal/sqlc/unmarshall.go
@@ -5,7 +5,6 @@ import (
 	"github.com/Polo123456789/entry-watch/internal/http/auth"
 )
 
-// unmarshall converts a SQLC User to an auth.User.
 func (u User) unmarshall() *auth.User {
 	return &auth.User{
 		ID:            u.ID,
@@ -18,15 +17,4 @@ func (u User) unmarshall() *auth.User {
 		Enabled:       u.Enabled,
 		Hidden:        u.Hidden,
 	}
-}
-
-// unmarshallSlice converts a slice of SQLC Users to a slice of auth.Users.
-//
-//nolint:unused // Available for future use when implementing user listing
-func unmarshallSlice(users []User) []*auth.User {
-	result := make([]*auth.User, len(users))
-	for i, u := range users {
-		result[i] = u.unmarshall()
-	}
-	return result
 }
